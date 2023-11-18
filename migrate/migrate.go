bde9bf4c7353f35adbba3299f2469c249076445e
mori-kei <mojavascript13@gmail.com>
2023-11-18 15:54:36 +0900

    Adminのマイグレーションの追加

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -10,5 +10,5 @@ func main() {
 	dbConn := db.NewDB()
 	defer fmt.Println("Successfully Migrated")
 	defer db.CloseDB(dbConn)
-	dbConn.AutoMigrate(&model.User{},&model.Profile{})
+	dbConn.AutoMigrate(&model.User{},&model.Profile{}, &model.Admin{})
 }
