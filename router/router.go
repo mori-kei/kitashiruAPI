d53dfaa45bd7afcc3f2a6579a95c99a3c7e30a0e
mori-kei <mojavascript13@gmail.com>
2023-12-03 21:13:27 +0900

    csrfの設定を変更

--- router/router.go
+++ router/router.go
@@ -59,20 +59,20 @@ func NewRouter(uc controller.IUserController, pc controller.IProfileController, 
 		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE"},
 		AllowCredentials: true,
 	}))
-	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
-		CookiePath:     "/",
-		CookieDomain:   os.Getenv("API_DOMAIN"),
-		CookieHTTPOnly: true,
+	// e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+	// 	CookiePath:     "/",
+	// 	CookieDomain:   os.Getenv("API_DOMAIN"),
+	// 	CookieHTTPOnly: true,
 
-		//Postmanで動作確認をする場合はsecure属性をfalseにする必要がある
-		//SameSiteをnonemodeにしてしまうと自動的にsecrureがonになるためPostmanで動作確認する時はsamasiteをDefaultModeに設定する]
-		//↓【通信用】フロントとの通信の際にはコメントアウトを消しPostmanで確認する際はコメントアウトする
-		CookieSameSite: http.SameSiteNoneMode,
-		//↓【API開発用】Postmanで確認する際はコメントアウトを消しフロントとの通信の際にはコメントアウトする
-		// CookieSameSite: http.SameSiteDefaultMode,
-		//CookieMaxAgeは秒単位で有効指定できる
-		//CookieMaxAge:   60,
-	}))
+	// 	//Postmanで動作確認をする場合はsecure属性をfalseにする必要がある
+	// 	//SameSiteをnonemodeにしてしまうと自動的にsecrureがonになるためPostmanで動作確認する時はsamasiteをDefaultModeに設定する]
+	// 	//↓【通信用】フロントとの通信の際にはコメントアウトを消しPostmanで確認する際はコメントアウトする
+	// 	// CookieSameSite: http.SameSiteNoneMode,
+	// 	//↓【API開発用】Postmanで確認する際はコメントアウトを消しフロントとの通信の際にはコメントアウトする
+	// 	CookieSameSite: http.SameSiteDefaultMode,
+	// 	//CookieMaxAgeは秒単位で有効指定できる
+	// 	//CookieMaxAge:   60,
+	// }))
 
 	//admin
 	a := e.Group("/admin")
