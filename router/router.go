e83049a5e4bf44b42088f218121549399ad0391a
mori-kei <mojavascript13@gmail.com>
2023-12-04 21:04:42 +0900

    samesiteをstrictmodeに変更

--- router/router.go
+++ router/router.go
@@ -63,7 +63,6 @@ func NewRouter(uc controller.IUserController, pc controller.IProfileController, 
 		CookiePath:     "/",
 		CookieDomain:   os.Getenv("API_DOMAIN"),
 		CookieHTTPOnly: true,
-
 		// Skipper: func(c echo.Context) bool {
 		// 	// 特定のルートを除外する場合などに使う、スキップする条件を設定
 
@@ -72,8 +71,8 @@ func NewRouter(uc controller.IUserController, pc controller.IProfileController, 
 		//Postmanで動作確認をする場合はsecure属性をfalseにする必要がある
 		//SameSiteをnonemodeにしてしまうと自動的にsecrureがonになるためPostmanで動作確認する時はsamasiteをDefaultModeに設定する]
 		//↓【通信用】フロントとの通信の際にはコメントアウトを消しPostmanで確認する際はコメントアウトする
-		CookieSameSite: http.SameSiteNoneMode,
-		CookieSecure:   true,
+		CookieSameSite: http.SameSiteStrictMode,
+		// CookieSecure:   true,
 		//↓【API開発用】Postmanで確認する際はコメントアウトを消しフロントとの通信の際にはコメントアウトする
 		// CookieSameSite: http.SameSiteDefaultMode,
 		//CookieMaxAgeは秒単位で有効指定できる
